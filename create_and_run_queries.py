e1602cfb6d567a56fd7b84495d1a3d2a8e9078f6
Sara Hamilton <sarahamilton@google.com>
2020-08-12 21:33:48 +0000

    added python script to create and run queries
    Change-Id: I9267ee3fadaeac9ef6e4d591acf1a3f5809adabc

---
+++ create_and_run_queries.py
@@ -0,0 +1,106 @@
+# /***********************************************************************
+# Copyright 2020 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Note that these code samples being shared are not official Google
+# products and are not formally supported.
+# ************************************************************************/
+import argparse
+import sys
+import os
+from datetime import datetime, timedelta
+from google.cloud import bigquery
+from google.cloud.bigquery import Table
+
+# Default parameters
+# Path to SQL queries. Update here if you have other queries
+working_directory = os.getcwd()
+feature_set_sql_path = "{}/sql/create_base_feature_set.sql".format(working_directory)
+model_sql_path = "{}/sql/train_logistic_regression.sql".format(working_directory)
+
+# Current timestamp (used to generate table names)
+current_timestamp = datetime.now().strftime("%Y_%m_%d_%H_%M")
+
+# Default outputs for datasets and tables. Update these if you want.
+feature_set_dataset = "ComcastGA360"
+feature_set_table = "Base_Feature_Set_{}".format(current_timestamp)
+
+# Base Feature Set parameters. This defaults to a one-year lookback and a 30-day scoring window
+start_date = "`{}`".format(datetime.now().strftime('%Y-%m-%d'))
+end_date = "`{}`".format((datetime.now() - timedelta(days=365)).strftime('%Y-%m-%d'))
+days_to_score = "30"
+
+# Model parameters
+remove_fields = "ClientId"
+model_dataset = "ComcastGA360"
+model_destination_table = "BQML_Model_Output_{}".format(current_timestamp)
+model_name = "{}.BQML_Model_{}".format(model_dataset, current_timestamp)
+
+def parse(argv):
+  parser = argparse.ArgumentParser(description='Accept Project ID, GA360 dataset, and GA360 Table.')
+  parser.add_argument('project_id', type=str, help='Your GCP Project ID')
+  parser.add_argument('ga_dataset', type=str, help='The dataset where you have your GA360 data')
+  parser.add_argument('ga_table', type=str, help='The table where you have your GA360 data')
+  args = parser.parse_args()
+  return args
+
+def read_query_from_file(file_path):
+  with open(file_path) as sqlFile:
+      sql = sqlFile.read()
+  return sql
+
+def create_and_run_feature_set_query(client, sql, ga_table_ref, destination_table):
+  sql = str(sql).replace("{start_date}", start_date).replace("{end_date}", end_date).replace("{ga_data_ref}", ga_table_ref).replace("{days_to_score}", days_to_score)
+  job_config = bigquery.QueryJobConfig(destination=destination_table)
+  query_job = client.query(sql, job_config=job_config)  # Make an API request.
+  query_job.result()  # Wait for the job to complete.
+
+  print("Query results loaded to the table {}".format(destination_table))
+
+def create_and_run_model_query(client, sql, feature_set_table_ref):
+  sql = str(sql).replace("{model_name}", model_name).replace("{remove_fields}", remove_fields).replace("{training_set}", feature_set_table_ref)
+  job_config = bigquery.QueryJobConfig()
+  query_job = client.query(sql, job_config=job_config)  # Make an API request.
+  query_job.result()  # Wait for the job to complete.
+
+  print("Model was created with the name {}".format(model_name))
+
+def main(argv):
+  args = parse(argv)
+  # Store CLI args
+  project_id = args.project_id
+  ga_dataset = args.ga_dataset
+  ga_table = args.ga_table
+
+  # Construct table references
+  ga_table_ref = "{}.{}.{}_*".format(project_id,ga_dataset,ga_table)
+  feature_set_table_ref = "{}.{}.{}".format(project_id,feature_set_dataset,feature_set_table)
+  model_table_ref = "{}.{}.{}".format(project_id,model_dataset,model_destination_table)
+
+  # Construct a BigQuery client object.
+  client = bigquery.Client(project=project_id)
+
+  # Create feature set query and query params and execute
+  feature_set_sql = read_query_from_file(feature_set_sql_path)
+  print("Feature set SQL has been read in.")
+  print(create_and_run_feature_set_query(client, feature_set_sql, ga_table_ref, feature_set_table_ref))
+
+  # Create model query and execute
+  model_sql = read_query_from_file(model_sql_path)
+  print("Model SQL has been read in")
+  print(create_and_run_model_query(client, model_sql, feature_set_table_ref))
+
+if __name__ == "__main__":
+  main(sys.argv)
+
